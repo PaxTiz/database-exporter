419d9d302200c0ca3a60b472a47f38b7e58992f0
Valentin Cernuta <vcernuta3@gmail.com>
2022-07-05 18:37:52 +0200

    feat: export dumps in a database-specific directory

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const { exec } = require('child_process')
 const { join } = require('path')
 const cron = require('node-cron')
+const { existsSync, mkdirSync } = require('fs')
 require('dotenv').config()
+
+const zeroes = value => {
+    value = value.toString()
+    return value.length === 2 ? value : `0${value}`
+}
 
 const envs = Object.keys(process.env).filter(e => e.startsWith('CRON_DATABASE_'))
 const tasks = []
@@ -19,8 +25,18 @@ for (const env of envs) {
 
 for (const task of tasks) {
     cron.schedule(task.schedule, () => {
-        const time = new Date().getTime()
-        const outputPath = join(__dirname, `${time}-${task.database}.sql`)
+        const fileDirectory = join(__dirname, 'dumps', task.database)
+        if (!existsSync(fileDirectory)) {
+            mkdirSync(fileDirectory, { recursive: true })
+        }
+
+        const date = new Date()
+        const day = zeroes(date.getDate())
+        const month = zeroes(date.getMonth())
+        const year = date.getFullYear()
+        const formattedDate = `${day}${month}${year}`
+
+        const outputPath = join(fileDirectory, `${formattedDate}-${task.database}.sql`)
         exec(`mysqldump -u${task.username} -p${task.password} ${task.database} > ${outputPath}`)
     }, { timezone: 'Europe/Paris', runOnInit: true })
 }
